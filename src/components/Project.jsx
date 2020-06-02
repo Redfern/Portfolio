60d5c30ee720b92694dacbfcfd54e3ebaede3849
Neil Redfern <redfern@gmail.com>
2020-06-02 16:42:10 +0100

    added electricity calculator project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,71 +1,77 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyledProject = styled.div`
-    margin-right: 20px;
-    width: 300px;
+  margin-right: 20px;
+  width: 300px;
+  margin: 0 auto;
+  margin-bottom: 20px;
+
+  h3 {
+    font-size: 20px;
+    font-weight: bold;
+    color: black;
+    text-align: center;
+  }
+
+  img {
+    max-height: 300px;
     margin: 0 auto;
-    margin-bottom: 20px;
+    display: block;
+    margin-bottom: 10px;
+    border-radius: 20px;
+  }
 
-    h3 {
-        font-size: 20px;
-        font-weight: bold;
-        color: black;
-        text-align: center;
+  .link {
+    width: 100%;
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  a {
+    color: #ffffff;
+    padding: 10px;
+    background-color: #00a7ff;
+    border-radius: 50px;
+    margin-top: 20px;
+    display: block;
+    margin: 0 auto;
+    width: 80%;
+    background: linear-gradient(to right, #1c92d0, #00a7ff);
+    position: relative;
+    text-decoration: none;
+    border: 1px solid transparent;
+
+    &:visited {
+      color: #ffffff;
     }
 
-    img {
-        max-height: 300px;
-        margin: 0 auto;
-        display: block;
-        margin-bottom: 10px;
-        border-radius: 20px;
+    &:hover {
+      background: linear-gradient(to right, white, white);
+      color: #00a7ff;
+      border: 1px solid #00a7ff;
     }
 
-    .link {
-        width: 100%;
-        text-align: center;
-        margin-top: 20px;
+    i {
+      position: absolute;
+      right: 13px;
+      top: 50%;
+      margin-top: -8px;
     }
-
-    a {
-        color: #ffffff;
-        padding: 10px;
-        background-color: #00a7ff;
-        border-radius: 50px;
-        margin-top: 20px;
-        display: block;
-        margin: 0 auto;
-        width: 80%;
-        background: linear-gradient(to right, #1c92d0, #00a7ff);
-        position: relative;
-        text-decoration: none;
-        border: 1px solid transparent;
-
-        &:hover {
-            background: linear-gradient(to right, white, white);
-            color: #00a7ff;
-            border: 1px solid #00a7ff;
-        }
-
-        i {
-            position: absolute;
-            right: 13px;
-            top: 50%;
-            margin-top: -8px;
-        }
-    }
+  }
 `;
 
-const Project = ({title, url, description, image}) => (
-    <StyledProject>
-        <img src={`/static/${image}`} alt={`for project ${title}`} />
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <div className="link">
-            <a href={url} target="_blank" rel="noopener noreferrer">Visit website <i className="fas fa-external-link-alt"></i></a>
-        </div>
-    </StyledProject>
+const Project = ({ title, url, description, image }) => (
+  <StyledProject>
+    <img src={`/static/${image}`} alt={`for project ${title}`} />
+    <h3>{title}</h3>
+    <p>{description}</p>
+    <div className="link">
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        Visit website <i className="fas fa-external-link-alt"></i>
+      </a>
+    </div>
+  </StyledProject>
 );
 
 export default Project;
